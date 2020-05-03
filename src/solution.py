0f9c1970802ca00092178659dac3f083bc611c99
azarrias <adolfo.zarrias@gmail.com>
2020-05-03 20:30:29 +0200

    Add utility functions for keeping track and recovering chronological data for visualization of puzzle solving

--- src/solution.py
+++ src/solution.py
@@ -12,21 +12,6 @@ unitlist = unitlist
 units = extract_units(unitlist, boxes)
 peers = extract_peers(units, boxes)
 
-def grid_values(grid):
-    """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
-
-    Args:
-        grid: Sudoku grid in string form, 81 characters long
-    Returns:
-        Sudoku grid in dictionary form:
-        - keys: Box labels, e.g. 'A1'
-        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
-    """
-    all_digits = '123456789'
-    values = [all_digits if c == '.' else c for c in grid]
-    assert len(values) == 81, "Input grid must be a string of length 81 (9x9)"
-    return dict(zip(boxes, values))
-
 def eliminate(values):
     """Eliminate values from peers of each box with a single value.
 
@@ -46,13 +31,20 @@ def eliminate(values):
     return values
 
 def only_choice(values):
-    """Finalize all values that are the only choice for a unit.
-
-    Go through all the units, and whenever there is a unit with a value
-    that only fits in one box, assign the value to this box.
-
-    Input: Sudoku in dictionary form.
-    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """Apply the only choice strategy to a Sudoku puzzle
+    The only choice strategy says that if only one box in a unit allows a certain
+    digit, then that box must be assigned that digit.
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+    Returns
+    -------
+    dict
+        The values dictionary with all single-valued boxes assigned
+    Notes
+    -----
+    You should be able to complete this function by copying your code from the classroom
     """
     for unit in unitlist:
         for digit in "123456789":
@@ -62,12 +54,16 @@ def only_choice(values):
     return values
 
 def reduce_puzzle(values):
-    """
-    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
-    If the sudoku is solved, return the sudoku.
-    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
-    Input: A sudoku in dictionary form.
-    Output: The resulting sudoku in dictionary form.
+    """Reduce a Sudoku puzzle by repeatedly applying all constraint strategies
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+    Returns
+    -------
+    dict or False
+        The values dictionary after continued application of the constraint strategies
+        no longer produces any changes, or False if the puzzle is unsolvable 
     """
     stalled = False
     while not stalled:
@@ -87,7 +83,21 @@ def reduce_puzzle(values):
     return values
 
 def search(values):
-    # Using depth-first search and propagation, create a search tree and solve the sudoku.
+    """Apply depth first search to solve Sudoku puzzles in order to solve puzzles
+    that cannot be solved by repeated reduction alone.
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+    Returns
+    -------
+    dict or False
+        The values dictionary with all boxes assigned or False
+    Notes
+    -----
+    You should be able to complete this function by copying your code from the classroom
+    and extending it to call the naked twins strategy.
+    """
     # First, reduce the puzzle using the previous function
     values = reduce_puzzle(values)
     if values is False:
@@ -106,12 +116,26 @@ def search(values):
         if result:
             return result
 
+def solve(grid):
+    """Find the solution to a Sudoku puzzle using search and constraint propagation
+    Parameters
+    ----------
+    grid(string)
+        a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    Returns
+    -------
+    dict or False
+        The dictionary representation of the final sudoku grid or False if no solution exists.
+    """
+    values = grid2values(grid)
+    values = search(values)
+    return values
+
 if __name__ == "__main__":
 #    sudoku_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
     sudoku_grid = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
-    values = grid_values(sudoku_grid) 
-    print("* Initial state:\n")
-    display(values)
-    values = search(values)
-    print("\n* Solved puzzle:\n")
-    display(values)
+    display(grid2values(sudoku_grid))
+    result = solve(sudoku_grid) 
+    display(result)
--- src/utils.py
+++ src/utils.py
@@ -60,11 +60,77 @@ def extract_peers(units, boxes):
                     peers[key_box].add(peer_box)
     return peers
 
-def display(values):
+def assign_value(values, box, value):
+    """You must use this function to update your values dictionary if you want to
+    try using the provided visualization tool. This function records each assignment
+    (in order) for later reconstruction.
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+    Returns
+    -------
+    dict
+        The values dictionary with the naked twins eliminated from peers
     """
-    Display the values as a 2-D grid.
-    Input: The sudoku in dictionary form
-    Output: None
+    # Don't waste memory appending actions that don't actually change any values
+    if values[box] == value:
+        return values
+
+    prev = values2grid(values)
+    values[box] = value
+    if len(value) == 1:
+        history[values2grid(values)] = (prev, (box, value))
+    return values
+
+def values2grid(values):
+    """Convert the dictionary board representation to as string
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+    Returns
+    -------
+    a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    """
+    res = []
+    for r in rows:
+        for c in cols:
+            v = values[r + c]
+            res.append(v if len(v) == 1 else '.')
+    return ''.join(res)
+
+def grid2values(grid):
+    """Convert grid into a dict of {square: char} with '123456789' for empties.
+    Parameters
+    ----------
+    grid(string)
+        a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    
+    Returns
+    -------
+        A grid in dictionary form
+            Keys: The boxes, e.g., 'A1'
+            Values: The value in each box, e.g., '8'. If the box has no value,
+            then the value will be '123456789'.
+    """
+    sudoku_grid = {}
+    for val, key in zip(grid, boxes):
+        if val == '.':
+            sudoku_grid[key] = '123456789'
+        else:
+            sudoku_grid[key] = val
+    return sudoku_grid
+
+def display(values):
+    """Display the values as a 2-D grid.
+    Parameters
+    ----------
+        values(dict): The sudoku in dictionary form
     """
     width = 1+max(len(values[s]) for s in boxes)
     line = '+'.join(['-'*(width*3)]*3)
@@ -72,4 +138,27 @@ def display(values):
         print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
                       for c in cols))
         if r in 'CF': print(line)
-    return
+    print()
+
+def reconstruct(values, history):
+    """Returns the solution as a sequence of value assignments 
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+    history(dict)
+        a dictionary of the form {key: (key, (box, value))} encoding a linked
+        list where each element points to the parent and identifies the value
+        assignment that connects from the parent to the current state
+    Returns
+    -------
+    list
+        a list of (box, value) assignments that can be applied in order to the
+        starting Sudoku puzzle to reach the solution
+    """
+    path = []
+    prev = values2grid(values)
+    while prev in history:
+        prev, step = history[prev]
+        path.append(step)
+    return path[::-1]
