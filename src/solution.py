2ebd337b855f9368ea40131e090b5f8a319b3118
azarrias <adolfo.zarrias@gmail.com>
2020-05-04 18:31:54 +0200

    Add diagonal units constraint (two board diagonals must not have any repeated values)

--- src/solution.py
+++ src/solution.py
@@ -3,10 +3,10 @@ from utils import *
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diagonal_units = [[rows[i]+cols[i] for i in range(len(rows))], 
+                  [rows[i]+cols[-1-i] for i in range(len(rows))]]
 
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+unitlist = row_units + column_units + square_units + diagonal_units
 
 # Must be called after all units (including diagonals) are added to the unitlist
 units = extract_units(unitlist, boxes)
